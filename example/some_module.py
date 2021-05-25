b1c84db1b3584da241b0dd5dea6d5009369cbbbe
matthijs <matthijs111@gmail.com>
2021-05-25 13:30:41 +0200

    example model with more computation time for clearer results

--- example/some_module.py
+++ example/some_module.py
@@ -4,11 +4,12 @@ from torch import nn
 class MyNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(100, 10, bias=False)
-        self.l2 = nn.Linear(10, 1, bias=False)
+        self.l1 = nn.Linear(100, 100, bias=False)
+        self.l2 = nn.Linear(100, 10, bias=False)
 
     def forward(self, x):
-        x = self.l1(x)
+        for _ in range(10):
+            x = self.l1(x)
         x = self.l2(x)
         return x
 
