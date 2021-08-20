fc89262697462dd341cd0828386b47c8eb5dd6d6
matthijs <matthijs111@gmail.com>
2021-08-20 02:26:52 +0200

    compensate time for num proc

--- experiments/plot_real_speedup_resnet18.py
+++ experiments/plot_real_speedup_resnet18.py
@@ -5,25 +5,30 @@ num_procs = np.arange(1, 33)
 
 results = []
 for num_proc in num_procs:
-    result = np.load(f"real_data/small_net_time_result_{num_proc}.npy") / num_proc
+    print(num_proc)
+    result = np.load(f"real_data/resnet18_time_result_{num_proc}.npy") / num_proc
     results.append(result)
 
 results = np.array(results)
+# plt.plot(results.T[1])
+# plt.show()
 
 seq_results = results[0]
 dist_results = results[1:]
 
 speedup = seq_results / dist_results
 
-speedup[speedup < 1] = np.nan
+# speedup[speedup < 1] = np.nan
 
 plt.imshow(speedup, cmap='plasma', origin='lower', aspect='auto',
-           extent=(1 - .5, 400 + .5, 2 - .5, max(num_procs) + .5))
+           extent=(1 - .5, 100 + .5, 2 - .5, max(num_procs) + .5))
 plt.colorbar(label='speedup')
 plt.xlabel('batch size')
 plt.ylabel('#MPI processes')
-plt.title('Real Speedup Small FCN')
-# plt.xticks(np.arange(min(x_values), max(x_values) + 1, 5))
+plt.title('Real Speedup ResNet18')
+# plt.xticks(np.arange(20, 40 + 1, 5))
 # plt.yticks(np.arange(min(y_values), max(y_values) + 1, 4))
 plt.grid(linewidth=0.3)
+plt.tight_layout()
+plt.savefig('resnet18.png')
 plt.show()
